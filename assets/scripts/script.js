65140ae059142e1a996f9a233b34c61ac5025fd1
RelentlessNC <njconklin1987@gmail.com>
2022-07-18 22:21:39 -0500

    trying to get rows created using JQuery in for loop

---
+++ assets/scripts/script.js
@@ -0,0 +1,43 @@
+var container = document.querySelector('.container');
+
+function gen_day(){
+    for (var i = 0; i <= 7; i++){
+        var row = $('div');
+        row.text("hello");
+        container.append(row);
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gen_day();
+
+
+
+
+
+
+
+
+
+
+
+
+//if ($ === undefined){
+//    console.log('JQuery not loaded');
+//}
+//
+//else{
+//    console.log('JQuery loaded');
+//}
